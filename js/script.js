818b980323ed2e48d413baba83e4ba198f19919c
Stefan Niedermann <info@niedermann.it>
2020-04-15 20:06:13 +0200

    Add corona warning

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     M.Sidenav.init(document.querySelectorAll('.sidenav'));
     M.Collapsible.init(document.querySelectorAll('.collapsible'));
-    M.Tooltip.init(document.querySelectorAll('.tooltipped'));
     if (!document.documentMode) { // https://github.com/Dogfalo/materialize/issues/5801
         M.Materialbox.init(document.querySelectorAll('.materialboxed'));
     }
